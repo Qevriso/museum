674e7663614422324569905e3cce8d7f3b252066
Qevriso <qevriso@gmail.com>
2025-12-11 23:58:56 +0100

    Update museum layout

---
+++ include.js
@@ -0,0 +1,10 @@
+document.querySelectorAll("[data-include]").forEach(async el => {
+    const file = el.getAttribute("data-include");
+    try {
+        const response = await fetch(file);
+        const html = await response.text();
+        el.innerHTML = html;
+    } catch (e) {
+        el.innerHTML = "<div style='color:red'>Ошибка загрузки секции: " + file + "</div>";
+    }
+});
